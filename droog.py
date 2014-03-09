d0c6fa4b54ad37159c4e4ea07c74d0b2da8fc375
Adam Miezianko <adam@theorylounge.org>
2014-03-08 22:33:39 -0800

    Rectilinear movement without boundry checking.

--- droog.py
+++ droog.py
@@ -19,5 +19,22 @@ hero.y = height / 2
 mainw.addch(hero.y, hero.x, ord("@"))
 mainw.move(hero.y, hero.x)
 
-mainw.getch()
+command = ' '
+
+while command != ord('q'):
+	command = mainw.getch()
+	old_hero_x, old_hero_y = hero.x, hero.y
+	if command == ord('h'):
+		hero.x -= 1
+	elif command == ord('l'):
+		hero.x += 1
+	elif command == ord('j'):
+		hero.y += 1
+	elif command == ord('k'):
+		hero.y -= 1
+	# draw the hero
+	mainw.addch(old_hero_y, old_hero_x, ord(' '))
+	mainw.addch(hero.y, hero.x, ord("@"))
+	mainw.move(hero.y, hero.x)
+
 curses.endwin()
