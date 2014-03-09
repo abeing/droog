20c0a7b507736c3662f42b042885a7f305e94874
Adam Miezianko <adam@theorylounge.org>
2014-03-08 22:42:06 -0800

    Diagonal movement and map boundries~

--- droog.py
+++ droog.py
@@ -1,5 +1,16 @@
 import curses
 
+class Map:
+	def __init__(self, height, width):
+		self.width = width
+		self.height = height
+	def isEmpty(self, y, x):
+		if y == 0 or y == height:
+			return False
+		if x == 0 or x == width:
+			return False
+		return True
+
 class Hero:
 	pass
 
@@ -14,6 +25,8 @@ curses.cbreak()
 height, width = mainw.getmaxyx()
 hero.x = width / 2
 hero.y = height / 2
+
+theMap = Map(height, width)
 
 # draw the hero
 mainw.addch(hero.y, hero.x, ord("@"))
@@ -32,6 +45,23 @@ while command != ord('q'):
 		hero.y += 1
 	elif command == ord('k'):
 		hero.y -= 1
+	elif command == ord('y'):
+		hero.x -= 1
+		hero.y -= 1
+	elif command == ord('u'):
+		hero.x += 1
+		hero.y -= 1
+	elif command == ord('b'):
+		hero.x -= 1
+		hero.y += 1
+	elif command == ord('n'):
+		hero.x += 1
+		hero.y += 1
+	
+	if not theMap.isEmpty(hero.y, hero.x):
+		hero.x = old_hero_x
+		hero.y = old_hero_y
+		
 	# draw the hero
 	mainw.addch(old_hero_y, old_hero_x, ord(' '))
 	mainw.addch(hero.y, hero.x, ord("@"))
