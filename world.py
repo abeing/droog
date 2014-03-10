455e5c251c5f0524183f55071af93cce1c112edb
Adam Miezianko <adam@theorylounge.org>
2014-03-09 21:55:12 -0700

    Began work on city map generator.

--- world.py
+++ world.py
@@ -1,3 +1,6 @@
+import random
+
+
 class World:
     def __init__(self, height, width):
         """Creates a World of the specified width and height.
@@ -11,13 +14,45 @@ class World:
             self.tiles.append(list())
             for x in range(width):
                 self.tiles[y].append(' ')
-                if y == 0 or y == height - 1:
-                    self.tiles[y][x] = '#'
-                elif x == 0 or x == width - 1:
-                    self.tiles[y][x] = '#'
+        self._generate()
 
     def isEmpty(self, y, x):
         """Returns True if the location is empty."""
         if (self.tiles[y][x] == ' '):
             return True
         return False
+
+    def _generate(self):
+        """Generates a city map.
+
+        We first draw a horizontal road somewhere near the equator of the map,
+        choosing the latitude using a triangular distribution with a mode of
+        the equator.
+
+        Then we draw a vertical road somewhere near the prime meridian of the
+        map, choosing the longitude using a triangular distribution with a
+        mode of the prime meridian.
+
+        Then we select an existing line."""
+
+        # We'll store the roads we create here.
+        roads = []
+
+        equator = int(random.triangular(low=0, high=self.height,
+                                        mode=self.height / 2))
+        for x in range(self.width):
+            self.tiles[equator][x] = '#'
+        roads.append(((equator, 0), (equator, self.width)))
+
+        meridian = int(random.triangular(low=0, high=self.width,
+                                         mode=self.width / 2))
+        for y in range(self.height):
+            self.tiles[y][meridian] = '#'
+        roads.append(((0, meridian), (self.height, meridian)))
+
+        for i in range(10):  # make ten additional roads for now
+            ((begin_y, begin_x), (end_y, end_x)) = random.choice(roads)
+            if (begin_y == end_y):  # We chose a horizontal road
+                # bisect = random.range(begin_x, end_x)
+                # direction = random.range(0, 2)
+                pass  # TODO: continue here
