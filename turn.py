f7d51a741b662bd3b8be66e319edb5252d1c88e5
Adam Miezianko <adam@theorylounge.org>
2014-07-24 20:46:25 +0000

    Fix passage of time.
    Time now only passes when every entity has had a turn to act for the turn. A
    Clock class is added to the turn queue which increments the current in-game
    time whenever its turn comes up.
    
    - Removed unused math import from UI module.
    - The game starts at 07:00:06, which is by design at the moment.
    
    Closes #14

--- turn.py
+++ turn.py
@@ -2,17 +2,48 @@ import datetime
 import logging
 import collections
 import sys
+import actor
 
 log = logging.getLogger(__name__)
+
+class Clock(actor.Actor):
+    """Tracks the current turn and in-game time."""
+    
+    def __init__(self):
+        self.current_turn = 0
+        self.SECONDS_PER_TURN = 6
+        self._current_time = datetime.datetime(100, 1, 1, 7, 0, 0)
+        self.ap = 0 
+        self.ap_max = 0 
+    
+    def current_time(self):
+        """Return the current in-game clock.
+
+        Each turn is some number of seconds.
+        """
+        return self._current_time.time().isoformat()
+
+    def act(self):
+        """When it is the clock's turn, it advances the time."""
+        self.current_turn += 1
+        self._current_time += datetime.timedelta(0, self.SECONDS_PER_TURN)
+        return 0
 
 class Turn:
     """Tracks the current turn and in-game time."""
 
     def __init__(self):
-        self.current_turn = 0
-        self.SECONDS_PER_TURN = 6
-        self._current_time = datetime.datetime(100, 1, 1, 7, 0, 0)
+        """Creates the turn actor queue and adds the clock to it.
+
+        After initialization, the turn actor queue only has one actor: the
+        clock. This means that the first actor of the first turn will be the
+        clock and time will advance. We could start the clock earlier than the
+        desired game start-time, or implement a no-op tick for the first tick,
+        but for now we'll delibrately leave it as is.
+        """
         self._queue = collections.deque()
+        self._clock = Clock()
+        self._queue.append(self._clock)
 
     def next(self):
         """Advances to the turn."""
@@ -22,20 +53,16 @@ class Turn:
         actor = self._queue.popleft()
         log.info("It is time for %r to act." % actor)
         if actor.act() < 1:
-            self.current_turn += 1
-            self._current_time += datetime.timedelta(0, self.SECONDS_PER_TURN)
             self._queue.append(actor)
             actor.refill()
         else:
             self._queue.appendleft(actor)
         return True
 
+        
     def current_time(self):
-        """Return the current in-game clock.
-
-        Each turn is some number of seconds.
-        """
-        return self._current_time.time().isoformat()
+        """Return the current in-game clock."""
+        return self._clock.current_time()
 
     def add_actor(self, actor):
         """Add a new actor to the end of the turn queue."""
--- ui.py
+++ ui.py
@@ -1,5 +1,4 @@
 import curses
-import math
 import logging
 import sys
 
