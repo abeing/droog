c636774abafb1a1e70ace4b0bac6c62fbfc7113c
Adam Miezianko <adam@theorylounge.org>
2015-04-01 09:26:12 -0700

    Remove clock.
    While adding unit tests for Turn, we found an issue with how the
    clock counts turns. The best fix was to refactor Turn to not use
    a Clock and instead track its ticks and time internally.

--- droog/test_turn.py
+++ droog/test_turn.py
@@ -1,18 +1,28 @@
 import turn as turn_
 import unittest
 import actor
+import logging
+
+logging.basicConfig()
 
 
 class TestActor(actor.Actor):
+    def __init__(self, name):
+        """The test actor counts how many times it's gone."""
+        self.count = 0
+        self.name = name
+
     def act(self):
-        """Does nothing."""
+        """Inrecement act count for 10 AP."""
+        self.count += 1
         return 10
 
 
-class EmptyTurnTestCase(unittest.TestCase):
+class ClockTurnTestCase(unittest.TestCase):
     def runTest(self):
         """A default Turn tracker should contain a clock."""
         turn = turn_.Turn()
+        turn.add_actor(TestActor("clock"))
         for t in range(100):
             turn.next()
         self.assertEqual(turn.current_time(), '07:01:40', "We should have"
@@ -24,9 +34,27 @@ class ManyActors(unittest.TestCase):
         """Add a hundred TestActors and run through a thousand next()s"""
         turn = turn_.Turn()
         for _ in range(100):
-            turn.add_actor(TestActor())
+            turn.add_actor(TestActor(""))
         for _ in range(1000):
             turn.next()
+
+
+class OrderActors(unittest.TestCase):
+
+    def setUp(self):
+        self.turn = turn_.Turn()
+        self.odd = TestActor("odd")
+        self.even = TestActor("even")
+        self.turn.add_actor(self.odd)
+        self.turn.add_actor(self.even)
+
+    def testEven(self):
+        for _ in range(100):
+            self.turn.next()
+        self.assertEqual(self.odd.count, 50, "Odd actor should have acted 50"
+                         " times.")
+        self.assertEqual(self.even.count, 50, "Even actor should have acted"
+                         " 50 times.")
 
 if __name__ == "__main__":
     unittest.main()
--- droog/turn.py
+++ droog/turn.py
@@ -10,33 +10,6 @@ LOG = logging.getLogger(__name__)
 SECONDS_PER_TURN = 1
 
 
-class Clock(actor.Actor):
-    """Tracks the current turn and in-game time."""
-
-    def __init__(self):
-        self.current_turn = 0
-        self._current_time = datetime.datetime(100, 1, 1, 7, 0, 0)
-
-    def current_time(self):
-        """Return the current in-game clock.
-
-        Each turn is some number of seconds.
-        """
-        return self._current_time.time().isoformat()
-
-    def act(self):
-        """When it is the clock's turn, it advances the time."""
-        self.current_turn += 1
-        self._current_time += datetime.timedelta(0, SECONDS_PER_TURN)
-        LOG.info("Turn %r start. Time is %r", self.current_turn,
-                 self.current_time())
-        return 0.9  # This is so the clock always go last on a given tick.
-
-    def __repr__(self):
-        """The clock's string representation is useful for logging purposes."""
-        return "the clock reading " + self.current_time()
-
-
 class Turn(object):
     """Tracks the current turn and in-game time."""
 
@@ -50,14 +23,18 @@ class Turn(object):
         but for now we'll delibrately leave it as is.
         """
         self._queue = Queue.PriorityQueue()
-        self._clock = Clock()
-        self.add_actor(self._clock)
+        self._current_turn = 0
+        self._current_time = datetime.datetime(100, 1, 1, 7, 0, 0)  # 07:00:00
 
     def next(self):
         """Advances to the turn."""
         if self._queue.qsize() == 0:
             LOG.error("Turn queue should never be empty!")
             sys.exit(1)
+
+        self._current_turn += 1
+        self._current_time += datetime.timedelta(seconds=SECONDS_PER_TURN)
+
         (_, an_actor) = self._queue.get()  # Ignoring priority
         LOG.info("It is time for %r to act.", an_actor)
         action_cost = an_actor.act()
@@ -65,7 +42,7 @@ class Turn(object):
         # check for death
         is_dead = getattr(an_actor, "is_dead", False)
         if not is_dead:
-            next_tick = action_cost + self._clock.current_turn
+            next_tick = action_cost + self._current_turn
             LOG.info("Requeueing actor %r at turn %r", an_actor,
                      next_tick)
             self._queue.put((next_tick, an_actor))
@@ -79,9 +56,9 @@ class Turn(object):
 
     def current_time(self):
         """Return the current in-game clock."""
-        return self._clock.current_time()
+        return self._current_time.time().isoformat()
 
     def add_actor(self, an_actor):
         """Add a new actor to the end of the turn queue."""
-        self._queue.put((self._clock.current_turn, an_actor))
+        self._queue.put((self._current_turn, an_actor))
         LOG.info("New actor in the turn queue: %r", an_actor)
