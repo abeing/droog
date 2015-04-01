db7dc6fdad4844a665d785e582b66d6d4ae28e1e
Adam Miezianko <adam@theorylounge.org>
2015-04-01 09:35:57 -0700

    Add action point test.

--- droog/test_turn.py
+++ droog/test_turn.py
@@ -7,15 +7,16 @@ logging.basicConfig()
 
 
 class TestActor(actor.Actor):
-    def __init__(self, name):
+    def __init__(self, name, increment=10):
         """The test actor counts how many times it's gone."""
         self.count = 0
         self.name = name
+        self.increment = increment
 
     def act(self):
         """Inrecement act count for 10 AP."""
         self.count += 1
-        return 10
+        return self.increment
 
 
 class ClockTurnTestCase(unittest.TestCase):
@@ -68,6 +69,24 @@ class OrderActors(unittest.TestCase):
         self.assertEqual(self.even.count, 49, "Even actor should have acted"
                          " 50 times.")
 
+
+class ActionCostTest(unittest.TestCase):
+
+    def setUp(self):
+        self.turn = turn_.Turn()
+        self.fast_actor = TestActor("fast", 1)
+        self.slow_actor = TestActor("slow", 100)
+
+    def testFast(self):
+        """The fast actor should act 99 times to the slow actor's one time."""
+        self.turn.add_actor(self.slow_actor)
+        self.turn.add_actor(self.fast_actor)
+        for _ in range(100):
+            self.turn.next()
+        self.assertEqual(self.fast_actor.count, 99,
+                         "Fast actor should have acted 99 times.")
+        self.assertEqual(self.slow_actor.count, 1,
+                         "Slow actor should have acted 1 time.")
 
 if __name__ == "__main__":
     unittest.main()
