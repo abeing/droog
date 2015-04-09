2e90918770eea7a0bc26940adf6161c94cc02d3c
Adam Miezianko <adam@theorylounge.org>
2015-04-08 20:50:27 -0700

    Change build to setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""A setuptools-based setup module for Droog."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='droog',
+
+    version='0.1.0',
+
+    description='A zombie roguelike',
+    long_description='A zombie roguelike',
+
+    url='https://github.com/abeing/droog',
+
+    author='Adam Miezianko',
+    author_email='adam@theorylounge.org',
+
+    license='None',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console :: Curses',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+        'Topic :: Games/Entertainment :: Role-Playing'
+        'Topic :: Games/Entertainment :: Turn Based Strategy'
+        ],
+
+    keywords='droog roguelike',
+
+    packages=find_packages(),
+
+    entry_points={
+        'console_scripts': [
+            'droog=droog:main',
+        ]
+    }
+)
